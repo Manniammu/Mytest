46d32b5b9e25c539f0870419e3bededefa151628
user <subithamanus@gmail.com>
2023-07-21 11:57:46 +0530

    First commit Uploading all files

---
+++ Gitprojct/src/First/FirstPro.java
@@ -0,0 +1,11 @@
+package First;
+
+public class FirstPro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="Hai welcome";
+System.out.println(s);
+	}
+
+}
